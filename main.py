f7dc8d0d2ceb4b112e17ea33ab67756650c56c9f
Rohith <rohithunairaus@gmail.com>
2026-06-08 14:40:54 +1000

    Added close event

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
-while True:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
