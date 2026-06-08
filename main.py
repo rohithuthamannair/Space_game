62f5a69f18e182ee5d2cb60178c629c004f9ebc1
Rohith <rohithunairaus@gmail.com>
2026-06-08 17:43:13 +1000

    Added the player image in to gaing window

--- main.py
+++ main.py
@@ -12,14 +12,27 @@ pygame.display.set_caption("Space Invadors")
 icon = pygame.image.load ('spaceship.png')
 pygame.display.set_icon(icon)
 
+#Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+     screen.blit(playerImg, (playerX, playerY))
 
 #Game Loop
 running = True
 while running:
+
+    #RGB- Red, Green, Blue
+    screen.fill ((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #RGB- Red, Green, Blue
-    screen.fill ((250, 0, 0))
+    
+    player()
+
     pygame.display.update()
